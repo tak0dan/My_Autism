b99faa51c8afa1b97a59ea60b1e26af4d2833187
Tak_0 on Thinkpad <tmtroshko@gmail.com>
2026-03-11 20:37:50 +0100

    ffzzhehshyysws3353g3g8d8djkehde777d7dj37sw3s3s3yu7

--- NixOS/nixos-build/nixorcist/listener.sh
+++ NixOS/nixos-build/nixorcist/listener.sh
@@ -2,7 +2,9 @@
 
 # Full terminal I/O listener (captures output + errors + keystrokes).
 # Set to 0 (or comment out the call in nixorcist.sh) to disable.
-NIXORCIST_LISTENER_ENABLED="${NIXORCIST_LISTENER_ENABLED:-1}"
+# Default off: interactive wrappers can interfere with fzf/read flows on some hosts.
+# Opt in by exporting NIXORCIST_LISTENER_ENABLED=1.
+NIXORCIST_LISTENER_ENABLED="${NIXORCIST_LISTENER_ENABLED:-0}"
 NIXORCIST_LISTENER_FILE="${NIXORCIST_LISTENER_FILE:-${ROOT:-.}/nixorcist-listener.txt}"
 NIXORCIST_LISTENER_TIMING="${NIXORCIST_LISTENER_TIMING:-${ROOT:-.}/nixorcist-listener.timing}"
 NIXORCIST_LISTENER_ACTIVE="${NIXORCIST_LISTENER_ACTIVE:-0}"
